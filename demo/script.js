2ddec0da59ba53c3a0c7813285ba61fb8b5e7106
jocando21 <jocando996@gmail.com>
2025-02-23 10:54:58 -0500

    Add separate demo page

---
+++ demo/script.js
@@ -0,0 +1,10 @@
+const progressBar = new WaveProgress("myCanvas", {
+    width: 300,
+    height: 100,
+    goalAmount: 100,
+    waterColor: ["#00aaff", "#004488"],
+    opacity: 0.8,
+    waveHeight: 6,
+    waveSpeed: 0.02,
+    counterFormat: "percentage"
+});
